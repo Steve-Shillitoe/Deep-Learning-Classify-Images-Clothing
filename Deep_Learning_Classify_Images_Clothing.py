3e3500b01768d4677b5f6b157458ebcd98ee09b5
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2023-08-13 23:07:50 +0100

    completed model and used it to predict

--- Deep_Learning_Classify_Images_Clothing.py
+++ Deep_Learning_Classify_Images_Clothing.py
@@ -13,6 +13,38 @@ import logging
 logger = tf.get_logger()
 logger.setLevel(logging.ERROR)
 
+def plot_image(i, predictions_array, true_labels, images):
+     predictions_array, true_label, img = predictions_array[i], true_labels[i], images[i]
+     plt.grid(False)
+     plt.xticks([])
+     plt.yticks([])
+ 
+     plt.imshow(img[...,0], cmap=plt.cm.binary)
+     predicted_label = np.argmax(predictions_array)
+     if predicted_label == true_label:
+        color = 'blue'
+     else:
+        color = 'red'
+ 
+     plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+     100*np.max(predictions_array),
+     class_names[true_label]),
+     color=color)
+
+
+def plot_value_array(i, predictions_array, true_label):
+ predictions_array, true_label = predictions_array[i], true_label[i]
+ plt.grid(False)
+ plt.xticks([])
+ plt.yticks([])
+ thisplot = plt.bar(range(10), predictions_array, color="#777777")
+ plt.ylim([0, 1])
+ predicted_label = np.argmax(predictions_array)
+ 
+ thisplot[predicted_label].set_color('red')
+ thisplot[true_label].set_color('blue')
+
+
 def normalize(images, labels):
     #Normalises pixel values in the range [0,1]
     images = tf.cast(images, tf.float32)
@@ -67,9 +99,9 @@ plt.show()
 
 model = tf.keras.Sequential([
  # Input layer
-    tf.keras.layers.Flatten(input_shape=(28, 28, 1)),
-    tf.keras.layers.Dense(128, activation=tf.nn.relu),
-    tf.keras.layers.Dense(10, activation='softmax')
+    tf.keras.layers.Flatten(input_shape=(28, 28, 1)), #input layer
+    tf.keras.layers.Dense(128, activation=tf.nn.relu), #hidden layer
+    tf.keras.layers.Dense(10, activation='softmax') #output layer
  ])
 
 #Compile the model
@@ -96,3 +128,49 @@ for test_images, test_labels in test_dataset.take(1):
 
 print('The confidence of the model that the image corresponds to each of the 10 different articles of clothing = {}'.format(predictions[0]))
 print('The most likely class = {}'.format(class_names[np.argmax(predictions[0])]))
+print('test_labels[0] = {}'.format(test_labels[0]))
+
+#Let's look at the 0th image, predictions, and prediction array.
+i = 0
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions, test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions, test_labels)
+
+i = 12
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions, test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions, test_labels)
+
+# Plot the first X test images, their predicted label, and the true label
+# Color correct predictions in blue, incorrect predictions in red
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+     plt.subplot(num_rows, 2*num_cols, 2*i+1)
+     plot_image(i, predictions, test_labels, test_images)
+     plt.subplot(num_rows, 2*num_cols, 2*i+2)
+     plot_value_array(i, predictions, test_labels)
+
+# Use the trained model to make a prediction about a single image
+
+# Grab an image from the test dataset
+img = test_images[0]
+print(img.shape)
+
+# Add the image to a batch where it's the only member.
+img = np.array([img])
+print(img.shape)
+
+predictions_single = model.predict(img)
+print(predictions_single)
+
+plot_value_array(0, predictions_single, test_labels)
+_ = plt.xticks(range(10), class_names, rotation=45)
+
+print('The most likely class = {}'.format(class_names[np.argmax(predictions_single[0])]))
